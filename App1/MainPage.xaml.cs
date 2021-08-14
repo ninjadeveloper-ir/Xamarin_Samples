9488c81da1381a87e9303853e578f97fd8136634
NPC <NPC@DESKTOP-BRHUHSI>
2021-08-14 19:34:05 +0430

    Add project files.

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+ 
+        public MainPage()
+        {
+            InitializeComponent();
+           
+        }
+      
+    }
+}
