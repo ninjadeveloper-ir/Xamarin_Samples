6f4d3f72b46f5b71fd8a63a0d6b45286a38d1468
vlearnsir <goftogocenter@gmail.com>
2021-08-14 22:30:07 +0430

    Add Weather_A UI - Using AbsoluteLayout

--- App1/App.xaml.cs
+++ App1/App.xaml.cs
@@ -11,7 +11,7 @@ namespace App1
         {
             InitializeComponent();
 
-            MainPage = new Weather();
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
